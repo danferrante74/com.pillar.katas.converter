fe3e0b8d5ac932c0eda9c3f343be0a79d03ac9ad
danferrante74 <danferrante74@gmail.com>
2015-10-29 11:28:39 -0400

    Added documentation

--- src/com/pillar/katas/converter/commands/ConverterCommand.java
+++ src/com/pillar/katas/converter/commands/ConverterCommand.java
@@ -7,25 +7,47 @@ public abstract class ConverterCommand<T> {
 	private ConverterTypes type;
 	private ConverterResult<T> result;
 	
-	
+	/**
+	 * 
+	 * @param type
+	 */
 	public ConverterCommand( ConverterTypes type )
 	{
 		this.type = type;
 	}
 	
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public ConverterTypes getType() {
 		return type;
 	}
 	
+	
+	/**
+	 * executes one of the converter commands
+	 * @throws ConverterExecutionException
+	 */
 	public abstract void execute() throws ConverterExecutionException;
 	
 	
+	
+	/**
+	 * 
+	 * @return ConverterResult<T> is the conversion result which allows us to grab a type safe result.
+	 */
 	public ConverterResult<T> getResult()
 	{
 		return this.result;
 	}
 	
 	
+	/**
+	 * 
+	 * @param result
+	 */
 	protected void setResult( ConverterResult<T> result )
 	{
 		this.result = result;
