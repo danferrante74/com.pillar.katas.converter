1ecbaae0a311aaa8cc274a29da02a078926b6e49
danferrante74 <danferrante74@gmail.com>
2015-10-28 20:30:54 -0400

    Added Tests

--- src/com/pillar/katas/converter/RomanNumeralConverter.java
+++ src/com/pillar/katas/converter/RomanNumeralConverter.java
@@ -1,10 +1,48 @@
 package com.pillar.katas.converter;
 
+
+/**
+ * This is the control layer...
+ *
+ */
 public class RomanNumeralConverter implements IConverterControl {
 
+	/**this here is the data model!!! It normally would be in another class.  view will never mess with this.**/
+	private static final int[] numericValues    = {1000, 900 , 500, 400 , 100, 90  , 50 ,  40 , 10 ,   9 ,  5 ,   4 ,  1};
+	private static final String[] numeralValues = {"M" , "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+	private static final int MAX_ROMAN_NUMERAL = 3999;
+	
+	public RomanNumeralConverter()
+	{
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see com.pillar.katas.converter.IConverterControl#convertTo(int)
+	 */
 	@Override
-	public String convertTo(int i) throws NotARomanNumeralException {
-		return "I";
+	public String convertTo( int x ) throws NotARomanNumeralException 
+	{		
+		if( x > MAX_ROMAN_NUMERAL || x <= 0 )
+			throw new NotARomanNumeralException();
+		
+		StringBuilder numeral = new StringBuilder();		
+		int runningTotal = x;
+		
+		for(int i=0; i<numericValues.length;)
+		{
+			int key = numericValues[i];
+			if( runningTotal - key >= 0 )
+			{
+				numeral.append( numeralValues[i] );
+				runningTotal = runningTotal - key;
+				continue;
+			}
+			++i;				
+		}			
+		return numeral.toString();
 	}
 
 }
--- test/com/pillar/katas/converter/TestRomanNumeralConverter.java
+++ test/com/pillar/katas/converter/TestRomanNumeralConverter.java
@@ -20,4 +20,116 @@ public class TestRomanNumeralConverter {
 			fail( "Value could not be represented as a roman numeral." );
 		}
 	}
+	
+	@Test
+	public void whenPassIn3GetRomanNumeral()
+	{
+		IConverterControl controller = new ConverterControlFactory().getConverter(ConverterTypes.ROMAN);
+		try
+		{
+			String romanNumeral = controller.convertTo( 3 );
+			System.out.println( romanNumeral );
+			assertEquals( "III", romanNumeral );
+		}
+		catch( NotARomanNumeralException ex )
+		{
+			fail( "Value could not be represented as a roman numeral." );			
+		}
+	}
+	
+
+	@Test
+	public void whenPassIn9GetRomanNumeral()
+	{
+		IConverterControl controller = new ConverterControlFactory().getConverter(ConverterTypes.ROMAN);
+		try
+		{
+			String romanNumeral = controller.convertTo( 9 );
+			System.out.println( romanNumeral );
+			assertEquals( "IX", romanNumeral );
+		}
+		catch( NotARomanNumeralException ex )
+		{
+			fail( "Value could not be represented as a roman numeral." );			
+		}
+	}
+	
+	@Test
+	public void whenPassIn1066ToGetRomanNumeral()
+	{
+		IConverterControl controller = new ConverterControlFactory().getConverter(ConverterTypes.ROMAN);
+		try
+		{
+			String romanNumeral = controller.convertTo( 1066 );
+			System.out.println( romanNumeral );
+			assertEquals( "MLXVI", romanNumeral );
+		}
+		catch( NotARomanNumeralException ex )
+		{
+			fail( "Value could not be represented as a roman numeral." );			
+		}
+	}	
+	
+	@Test
+	public void whenPassIn1989ToGetRomanNumeral()
+	{
+		IConverterControl controller = new ConverterControlFactory().getConverter(ConverterTypes.ROMAN);
+		try
+		{
+			String romanNumeral = controller.convertTo( 1989 );
+			System.out.println( romanNumeral );
+			assertEquals( "MCMLXXXIX", romanNumeral );
+		}
+		catch( NotARomanNumeralException ex )
+		{
+			fail( "Value could not be represented as a roman numeral." );			
+		}
+	}
+	
+	@Test
+	public void whenPassInAValueGreaterThanMaxRomanNumeral()
+	{
+		IConverterControl controller = new ConverterControlFactory().getConverter(ConverterTypes.ROMAN);
+		try
+		{
+			controller.convertTo( 4000 );
+			fail( "!!!YOU MESSED UP: There is no roman numeral that can be represented here!!!" );
+		}
+		catch( NotARomanNumeralException ex )
+		{
+			assertTrue( true );
+		}
+		
+	}
+	
+	@Test
+	public void whenPassInAValueLessThan0ToGetRomanNumeral()
+	{
+		IConverterControl controller = new ConverterControlFactory().getConverter(ConverterTypes.ROMAN);
+		try
+		{
+			controller.convertTo( -4000 );
+			fail( "!!!YOU MESSED UP: There is no roman numeral that can be represented here!!!" );
+		}
+		catch( NotARomanNumeralException ex )
+		{
+			assertTrue( true );
+		}
+		
+	}	
+	
+	@Test
+	public void whenPassInA0ToGetRomanNumeral()
+	{
+		IConverterControl controller = new ConverterControlFactory().getConverter(ConverterTypes.ROMAN);
+		try
+		{
+			controller.convertTo( 0 );
+			fail( "!!!YOU MESSED UP: There is no roman numeral that can be represented here!!!" );
+		}
+		catch( NotARomanNumeralException ex )
+		{
+			assertTrue( true );
+		}		
+	}	
 }
